2cb09eef64abc29eeeae5983e98529fddc0a872b
Hayden Carlson <haydog2@hotmail.com>
2016-11-10 19:56:45 -0800

    Adding email send after successfull checkout

---
+++ app/mailers/receipt_mailer.rb
@@ -0,0 +1,8 @@
+ class ReceiptMailer < ApplicationMailer
+  # default from: ''
+
+  def receipt_email(order)
+    @order = order
+    mail(to: @order.email, subject: "Your order receipt for #{@order.id}")
+  end
+end 
