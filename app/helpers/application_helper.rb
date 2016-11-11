fd0657858d9e40467367237040796b9a31bf9680
April Mulvey <april.mulvey@gmail.com>
2016-11-11 02:11:39 +0000

    have your ordered item image,quantity,total all showing up on order/show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
   def current_user
     User.find(session[:user_id]) if session[:user_id]
   end
+
+  # def cart_total
+  #   @cart_total =
+  # end
+
+  def cart_total
+    total = 0
+    cart.each do |product_id, details|
+      if p = Product.find_by(id: product_id)
+        total += p.price_cents * details['quantity'].to_i
+      end
+    end
+    total
+  end
 end
