8ca0dc834ad21081e0e134550d81b5d9a4350242
April Mulvey <april.mulvey@gmail.com>
2016-11-16 01:16:25 +0000

    changed it so people can order without being logged in

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,9 +37,8 @@ class OrdersController < ApplicationController
   end
 
   def create_order(stripe_charge)
-
     order = Order.new(
-      email: current_user.email,
+      email: current_user.email || "",
       total_cents: cart_total,
       stripe_charge_id: stripe_charge.id, # returned by stripe
     )
